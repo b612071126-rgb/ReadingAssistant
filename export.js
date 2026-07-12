dfd74ea4bb41c9a2c3ce85c11abd65011ed99e24
b612071126-rgb <b612071126@gmail.com>
2026-07-12 21:57:25 +0800

    更新 export.js

--- export.js
+++ export.js
@@ -1,1 +1,34 @@
-1
+const STORAGE_KEY = "records";
+
+
+// 保存阅读记录
+
+function saveRecords(records){
+
+localStorage.setItem(
+STORAGE_KEY,
+JSON.stringify(records)
+);
+
+}
+
+
+
+//读取阅读记录
+
+function loadRecords(){
+
+let data =
+localStorage.getItem(STORAGE_KEY);
+
+
+if(data){
+
+return JSON.parse(data);
+
+}
+
+
+return [];
+
+}
