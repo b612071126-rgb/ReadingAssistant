a514ff98ea03f65accfd882cbe92301a7fc55200
b612071126-rgb <b612071126@gmail.com>
2026-07-13 12:17:36 +0800

    更新 reader.js

--- reader.js
+++ reader.js
@@ -1,12 +1,13 @@
 let currentArchive = null;
 
 
+// 开始阅读
 
 function startReading(){
 
     currentArchive = {
 
-        id:Date.now(),
+        id: Date.now(),
 
         title:"",
 
@@ -26,44 +27,102 @@ function startReading(){
 
     };
 
+
+    console.log("开始阅读", currentArchive);
+
 }
 
 
 
-function addExcerpt(text){
+// 添加摘录
+
+function addExcerpt(content){
+
+    if(!currentArchive){
+
+        alert("请先开始阅读");
+
+        return;
+
+    }
+
 
     currentArchive.excerpts.push({
 
-        content:text,
+        content:content,
 
         time:new Date()
 
     });
 
+
 }
 
 
 
-function addThought(text){
+// 添加思考
+
+function addThought(content){
+
+    if(!currentArchive){
+
+        alert("请先开始阅读");
+
+        return;
+
+    }
+
 
     currentArchive.thoughts.push({
 
-        content:text,
+        content:content,
 
         time:new Date()
 
     });
 
+
 }
 
 
 
+// 结束阅读
+
 function finishReading(){
+
+
+    if(!currentArchive){
+
+        alert("当前没有阅读");
+
+        return null;
+
+    }
+
+
 
     currentArchive.endTime=new Date();
 
 
-    //计算时间
+
+    let time=
+
+    currentArchive.endTime
+
+    -
+
+    currentArchive.startTime;
+
+
+
+    currentArchive.duration=
+
+    Math.floor(time/1000/60);
+
+
+
+    console.log("阅读结束",currentArchive);
+
 
 
     return currentArchive;
