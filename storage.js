6632e80529d5596942be20455be17fad26a1a2a1
b612071126-rgb <b612071126@gmail.com>
2026-07-13 16:43:32 +0800

    更新 storage.js

--- storage.js
+++ storage.js
@@ -1,1 +1,35 @@
-1
+const STORAGE_KEY = "readingArchives";
+
+
+// 保存阅读档案
+
+function saveArchives(archives){
+
+    localStorage.setItem(
+        STORAGE_KEY,
+        JSON.stringify(archives)
+    );
+
+}
+
+
+
+// 读取阅读档案
+
+function loadArchives(){
+
+    let data = localStorage.getItem(
+        STORAGE_KEY
+    );
+
+
+    if(data){
+
+        return JSON.parse(data);
+
+    }
+
+
+    return [];
+
+}
