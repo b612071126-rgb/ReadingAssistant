6b8b86eb527e5df6e82933cc8f430fdca23e6ac4
b612071126-rgb <b612071126@gmail.com>
2026-07-14 16:29:08 +0800

    更新 app.js

--- app.js
+++ app.js
@@ -19,6 +19,64 @@ let records = loadArchives();
 
 
 
+// 迁移旧数据
+
+let oldRecords =
+JSON.parse(localStorage.getItem("records"))
+|| [];
+
+
+if(
+    oldRecords.length > 0 &&
+    records.length === 0
+){
+
+    records = oldRecords.map(item=>{
+
+
+        return {
+
+            id:item.id,
+
+            title:item.title,
+
+            source:item.source,
+
+            startTime:new Date(),
+
+            endTime:new Date(),
+
+            duration:item.time || 0,
+
+            excerpts:[
+                {
+                    content:item.quote || "",
+                    time:new Date()
+                }
+            ],
+
+            thoughts:[
+                {
+                    content:item.thought || "",
+                    time:new Date()
+                }
+            ],
+
+            tags:[]
+
+        };
+
+
+    });
+
+
+    saveArchives(records);
+
+
+}
+
+
+
 function startTimer(){
 
     startReading();
