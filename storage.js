0fb59c9c8c0b9dfa71d1496454e4e334c65acf7a
b612071126-rgb <b612071126@gmail.com>
2026-07-15 19:53:32 +0800

    更新 storage.js

--- storage.js
+++ storage.js
@@ -1,23 +1,36 @@
+// ==========================
+// 阅读档案存储
+// ==========================
+
+
 const STORAGE_KEY = "readingArchives";
 
 const NOTE_KEY = "quickNotes";
 
 
-// ======================
-// 阅读档案
-// ======================
+
+
 
 
 // 保存阅读档案
 
 function saveArchives(archives){
 
+
     localStorage.setItem(
+
         STORAGE_KEY,
+
         JSON.stringify(archives)
+
     );
 
+
 }
+
+
+
+
 
 
 
@@ -25,52 +38,143 @@ function saveArchives(archives){
 
 function loadArchives(){
 
+
     let data =
-    localStorage.getItem(STORAGE_KEY);
+    localStorage.getItem(
+        STORAGE_KEY
+    );
 
 
-    if(data){
 
-        return JSON.parse(data);
+    if(!data){
+
+        return [];
 
     }
 
 
-    return [];
+
+    try{
+
+
+        return JSON.parse(data);
+
+
+
+    }
+    catch(e){
+
+
+        console.log(
+            "档案数据错误，重新初始化"
+        );
+
+
+        return [];
+
+    }
+
 
 }
 
 
 
-// ======================
-// 速记
-// ======================
+
+
+
+
+
+
+// 保存速记
 
 
 function saveNotes(notes){
 
+
+
     localStorage.setItem(
+
         NOTE_KEY,
+
         JSON.stringify(notes)
+
     );
+
 
 }
 
 
 
+
+
+
+
+// 读取速记
+
+
 function loadNotes(){
 
+
     let data =
-    localStorage.getItem(NOTE_KEY);
+    localStorage.getItem(
+        NOTE_KEY
+    );
 
 
-    if(data){
 
-        return JSON.parse(data);
+    if(!data){
+
+        return [];
 
     }
 
 
-    return [];
+
+
+    try{
+
+
+        return JSON.parse(data);
+
+
+
+    }
+    catch(e){
+
+
+        return [];
+
+    }
+
+
+
+}
+
+
+
+
+
+
+
+// 清空所有数据
+// 测试使用
+
+function clearAllData(){
+
+
+    localStorage.removeItem(
+        STORAGE_KEY
+    );
+
+
+    localStorage.removeItem(
+        NOTE_KEY
+    );
+
+
+    alert(
+        "数据已清空"
+    );
+
 
 }
